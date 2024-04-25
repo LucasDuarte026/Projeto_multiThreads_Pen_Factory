e41103b243d49f0d9059b1b8434c004d3ab91b2b
LucasDuarte026 <luska026@LAPTOP-POCBUK79>
2024-04-24 23:54:05 -0300

    código semi finalizado. Inserção dos arquivos velhos e da bateria de testes

---
+++ testes/testes_exec.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+./main < testes/1.in > resultados/1.out &
+./main < testes/2.in > resultados/2.out &
+./main < testes/3.in > resultados/3.out &
+./main < testes/4.in > resultados/4.out &
+./main < testes/5.in > resultados/5.out &
+./main < testes/6.in > resultados/6.out &
+./main < testes/7.in > resultados/7.out &
+./main < testes/8.in > resultados/8.out &
+./main < testes/9.in > resultados/9.out &
+./main < testes/10.in > resultados/10.out &
